b35c9efc155a40f09c1b399280c28bf70769af6c
KaybieRocket <kabrokaw@gmail.com>
2014-04-06 18:38:22 -0700

    done

--- app/js/client.js
+++ app/js/client.js
@@ -4,7 +4,19 @@
 angular.module('client', ['stripe'])
     .controller('MainCtrl', ['$scope', '$http', function ($scope, $http) {
         'use strict';
-        $scope.mySillyText = "Welcome to Brian's Awesome Products";
+        $scope.mySillyText = "Welcome to the mind of K.B. Rocket";
+        $scope.priceMessage = "";
+        $scope.bikeColor = "";
+        $scope.popAlert = function () {
+            alert('i do not want to say hello. i cannot stand the hello world stuff and etc.')
+        };
+        $scope.checkPrice = function (bikeColor) {
+            $http.get('/checkPrice', {params: {bikeColor: bikeColor}})
+                .success(function (response) {
+                    $scope.bikeColor = bikeColor;
+                    $scope.priceMessage = response.availability;
+                });
+        };
         $scope.saveCustomer = function stripeResponseHandler(status, response) {
             if (response.error) {
                 // show the errors on the form
